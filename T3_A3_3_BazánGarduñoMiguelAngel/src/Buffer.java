41651a6ae9431a0f4c91bb8f47630ac4e4fc1c8d
Miguel Bazán <mabg211299@hotmail.com>
2020-03-20 19:00:49 -0600

    Se añadio codigoo sincronizacion Productor Consumidor Con array

---
+++ T3_A3_3_BazánGarduñoMiguelAngel/src/Buffer.java
@@ -0,0 +1,8 @@
+
+public interface Buffer{ 
+	 // place int value into Buffer 6   
+	 public void blockingPut(int value) throws InterruptedException; 
+	  // return int value from Buffer 9   
+	 public int blockingGet() throws InterruptedException; 
+	 
+	 } // end interface
